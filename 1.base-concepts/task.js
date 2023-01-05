83a85d9f670284102ed0c09eeba32306e648fffe
TatyanaMilyutkina <milytkinata@gmail.com>
2023-01-05 23:53:27 +0400

    Inicial commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,33 @@
 "use strict"
-function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
-}
+let root1 , root2;
+let a = 1;
+let b = 12;
+let c = 36;
+function solveEquation (a, b, c) 
+{
+    let arr = [];
+    let discriminant = b * b - 4 * a * c;
+    if (discriminant > 0) {
+        root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+        root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+    
+        console.log(`The roots of quadratic equation are ${root1} and ${root2}`);
+    }
+    else if (discriminant == 0) {
+        root1 = root2 = -b / (2 * a);
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+        console.log(`The roots of quadratic equation are ${root1} and ${root2}`);
+    }
+    else {
+        let realPart = (-b / (2 * a)).toFixed(2);
+        let imagPart = (Math.sqrt(-discriminant) / (2 * a)).toFixed(2);
+    
+        // result
+        console.log(
+        `The roots of quadratic equation are ${realPart} + ${imagPart}i and ${realPart} - ${imagPart}i`
+      );
+    }
+    
+    
+    return arr;
 }
