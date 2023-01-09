a51f43f87f3d43286a82b26f17eba444b6afd3ad
TatyanaMilyutkina <milytkinata@gmail.com>
2023-01-09 09:54:51 +0400

    correction of 09.01.2023

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,32 +1,15 @@
 "use strict"
-let root1 , root2;
-let a = 1;
-let b = 12;
-let c = 36;
-function solveEquation (a, b, c) 
-{
-    let arr = [];
-    let discriminant = b * b - 4 * a * c;
-    if (discriminant > 0) {
-        root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
-        root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
-    
-        console.log(`The roots of quadratic equation are ${root1} and ${root2}`);
-    }
-    else if (discriminant == 0) {
-        root1 = root2 = -b / (2 * a);
+function solveEquation(a, b, c) {
+  let D = (b * b) - 4 * a * c;
+  let arr = [];
 
-        console.log(`The roots of quadratic equation are ${root1} and ${root2}`);
-    }
-    else {
-        let realPart = (-b / (2 * a)).toFixed(2);
-        let imagPart = (Math.sqrt(-discriminant) / (2 * a)).toFixed(2);
-    
-        console.log(
-        `The roots of quadratic equation are ${realPart} + ${imagPart}i and ${realPart} - ${imagPart}i`
-      );
-    }
-    
-    
-    return arr;
+  if (D < 0) {
+    arr = [];
+  }
+  if (D == 0) {
+    arr = [`${-b / (2 * a)}`]
+  } else {
+    arr = [`${(-b + Math.sqrt(D)) / (2 * a)}`, `${(-b - Math.sqrt(D)) / (2 * a)}`]
+  }
+  return arr;
 }
