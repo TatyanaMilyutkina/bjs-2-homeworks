580f7dc7b0231f9ec384ad60c26131aaf36d662c
TatyanaMilyutkina <milytkinata@gmail.com>
2023-01-10 10:39:33 +0400

    Modif Task 1 (10.01.2023)

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -3,13 +3,10 @@ function solveEquation(a, b, c) {
   let D = (b * b) - 4 * a * c;
   let arr = [];
 
-  if (D < 0) {
-    arr = [];
-  }
   if (D == 0) {
-    arr = [`${-b / (2 * a)}`]
-  } else {
-    arr = [`${(-b + Math.sqrt(D)) / (2 * a)}`, `${(-b - Math.sqrt(D)) / (2 * a)}`]
+    arr = [-b / (2 * a)]
+  } else if (D > 0) {
+    arr = [(-b + Math.sqrt(D)) / (2 * a), (-b - Math.sqrt(D)) / (2 * a)]
   }
   return arr;
 }
--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,24 +1,26 @@
 function getArrayParams(...arr) {
+  let min;
+  let max;
+  let sum = 0;
+  let avg;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr;
 
-  return { min: min, max: max, avg: avg };
+  }
+  avg = sum / i;
+  return { avg: avg };
+  //return { min: min, max: max, avg: avg }; вольный стиль 8 937 064 76 44 Журавлева Анна (450*(3)*12) пн 15 часов  среда 17.15 пятница 17.15 
 }
+getArrayParams(-99, 99, 10) // { min: -99, max: 99, avg: 3.33 }
+//getArrayParams(1, 2, 3, -100, 10)  // { min: -100, max: 10, avg: -16.80 }
+//getArrayParams(5)  // { min: 5, max: 5, avg: 5 }
 
-function summElementsWorker(...arr) {
+//function summElementsWorker(...arr) {}
 
-}
+//function differenceMaxMinWorker(...arr) {}
 
-function differenceMaxMinWorker(...arr) {
+//function differenceEvenOddWorker(...arr) {}
 
-}
+//function averageEvenElementsWorker(...arr) {}
 
-function differenceEvenOddWorker(...arr) {
-
-}
-
-function averageEvenElementsWorker(...arr) {
-
-}
-
-function makeWork (arrOfArr, func) {
-
-}
+//function makeWork(arrOfArr, func) {}
