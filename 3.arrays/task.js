c21c01103fe423e1a1ee64a4a44097ebb9184047
TatyanaMilyutkina <milytkinata@gmail.com>
2023-01-13 10:08:23 +0400

    Arrays (13.01.2023)

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,12 @@
 function compareArrays(arr1, arr2) {
-  
+    let result = arr1.length === arr2.length && arr1.every((n, i) => n === arr2[i]);
+    return result;
 }
 
-function getUsersNamesInAgeRange(users, gender) {
-  
+
+function advancedFilter(arr) {
+    let resultArr;
+    let result = arr.filter(n => n > 0).filter(n => n % 3 === 0).map(n => n * 10)
+
+    return result;
 }
